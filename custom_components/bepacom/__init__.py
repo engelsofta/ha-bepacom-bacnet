991bcb09d4e67fae4b35d8dca878a2a11866f4d5
engelsoft <52287633+engelsofta@users.noreply.github.com>
2026-07-05 16:31:57 +0200

    refactor: Add all platform types to integration setup

--- custom_components/bepacom/__init__.py
+++ custom_components/bepacom/__init__.py
@@ -13,7 +13,7 @@ from .coordinator import BepacomCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS: list[str] = ["sensor"]
+PLATFORMS: list[str] = ["sensor", "binary_sensor", "switch", "number"]
 
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
